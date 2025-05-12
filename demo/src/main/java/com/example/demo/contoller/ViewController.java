197d6a8fb2192fac0f6453987317ebc24a6d5e09
Yuvraj Verma <varma.yash18@gmail.com>
2025-05-12 10:29:50 +0530

    adding JSP page in our sts

---
+++ demo/src/main/java/com/example/demo/contoller/ViewController.java
@@ -0,0 +1,13 @@
+package com.example.demo.contoller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ViewController {
+	@RequestMapping("/home")
+	public String home() {
+		return "index";
+	}
+
+}
