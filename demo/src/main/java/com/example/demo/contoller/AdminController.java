c546b41aff19f51ab8ef62adc6e0ea57f1914e55
Yuvraj Verma <varma.yash18@gmail.com>
2025-05-15 09:54:38 +0530

    Learned Stereotype Annotations

---
+++ demo/src/main/java/com/example/demo/contoller/AdminController.java
@@ -0,0 +1,23 @@
+package com.example.demo.contoller;
+import org.springframework.context.annotation.Bean;
+
+import com.example.demo.entity.User;
+
+import jakarta.persistence.Entity;
+
+
+import org.springframework.stereotype.Controller;
+@Controller
+public class AdminController {
+	public AdminController() {
+		System.out.println("Admin Controller ");
+	}
+	@Bean
+	public User getUser() {
+		System.out.println("User Object");
+		return new User();
+	}
+}
+
+
+
