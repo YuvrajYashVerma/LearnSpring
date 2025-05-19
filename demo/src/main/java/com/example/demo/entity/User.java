a5bb749083aa89f7149af574f486e409fc3815df
Yuvraj Verma <varma.yash18@gmail.com>
2025-05-19 21:45:04 +0530

    working of @GeneratedValue(strategy = GenerationType.IDENTITY)

--- demo/src/main/java/com/example/demo/entity/User.java
+++ demo/src/main/java/com/example/demo/entity/User.java
@@ -3,6 +3,7 @@ package com.example.demo.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class User {
---
+++ demo/src/main/java/com/example/demo/entity/User2.java
@@ -0,0 +1,62 @@
+package com.example.demo.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+@Entity
+public class User2 {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+     private Integer id;
+    @Column 
+	private String name;
+    @Column 
+    private String username;
+    @Column 
+    private String password;
+     
+     public User2() {
+    	 
+     }
+     public User2(Integer id, String name, String username, String password) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.username = username;
+		this.password = password;
+	}
+	@Override
+	public String toString() {
+		return "User2 [id=" + id + ", name=" + name + ", username=" + username + ", password=" + password + "]";
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
